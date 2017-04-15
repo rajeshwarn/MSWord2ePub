16058e4e1e73b651dc42b33f15e0224f61834d67
phfrancois <phfrancois@users.sourceforge.net>
2017-04-15 23:12:40 +0200

    emove unnecesary files

--- Signature.bat
+++
@@ -1,3 +0,0 @@
-:: depuis invite de commande développeur :
-signtool sign /f "\\FDLNSERVER\Shared Folders\Documents\2014 09 13 Sauvegarde\Documents\Visual Studio 2010\Projects\SourceForge\Word2HTML4ePub\Word2HTML4ePub\Word2ePub.pfx" /p 200178 "\\FDLNSERVER\Shared Folders\Documents\2014 09 13 Sauvegarde\Documents\Visual Studio 2010\Projects\SourceForge\Word2HTML4ePub\Setup Word2ePub_2015\Release\Setup Word2ePub_2015.msi"
-pause
